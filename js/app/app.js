16cda93ad2e19328b6dfdfc072d25f1ff207b627
saligaros <iasonasgram85@gmail.com>
2023-12-22 15:24:49 +0200

    first changes with john's help

--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,21 @@
-alert("Welcome Bookaholics Happy Coding")
+function searchBooks (queryString) {
+    return fetch(`https://www.googleapis.com/books/v1/volumes?maxResults=10&q=${queryString}`)
+        .then((res)=>res.json()) 
+        .then((res)=>res.json()) 
+
+ 
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const $form = document.querySelector("#form"); 
+    $form.addEventListener("submit", (e) => {
+        e.preventDefault(); 
+        const queryInput = document.querySelector('#query');
+        console.log(queryInput.value);
+        const queryString = queryInput.value;
+    });
+})
+
+
+
+
