0ba6ab75060a799cc928f3db3d108aecd557b168
unknown <koundouras@gmail.com>
2024-01-18 20:12:16 +0200

    changed books.js

--- js/app/books.js
+++ js/app/books.js
@@ -1,8 +1,11 @@
+const demoText = "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. vestibulum tortor quam, feugiat vitae, ultricies.";
+
 // Function to generate the template with variables
 function generateTemplate(item, index) {
 	const bookClass = `book-${index+1}`;
-    const template =  `		
+    const template =  `	
             <figure>
+			<h4><span>Book id: </span><span>${item.id}</span></h4>	
 				<div class="perspective">
 					<div class="book ${bookClass}" data-book="book-${index+1 || 0}">
 						<div class="cover">
@@ -16,9 +19,9 @@ function generateTemplate(item, index) {
                 <figcaption><h2>${item.title}<span>${item.sale}</span><span>${item.averageRating}</span></h2></figcaption>
                 <div class="details">
 					<ul>
-						<li>${item.subtitle}</li>
+						<li>${item.subtitle || demoText}</li>
 						<li>${item.authors}</li>
-						<li>${ item.publisheDate}</li>
+						<li>${item.publishDate}</li>
 						<li>${item.pageCount} pages</li>
 					</ul>
 				</div>
@@ -27,15 +30,21 @@ function generateTemplate(item, index) {
 	return template;
 }
 
+
 function generateContentTemplate(item, index) {
     return `	
 	<div class="bb-custom-wrapper" id="book-${index+1 || 0}">
 		<div class="bb-bookblock bb-vertical">
 			<div class="bb-item" style="display: block;">
 				<div class="bb-custom-side page-layout-3">
-					<div>
-						<h3>${item.name}</h3>
-						<p>${item.email}</p>
+					<div id="book-content-top-left">
+						<h3>book content</h3>
+					</div>
+					<div id="book-content-middle-left">
+						<h4><span>Isbn: </span><span>${item.extractISBN}</span></h4>
+						<h3>${item.title}</h3>
+						<span>${item.searchInfo.length > 0 ? item.searchInfo : (item.subtitle || demoText)}</span>
+						<button id="readmore"><span>more</span><img src="assets/images/chevron-right.svg"/></button>
 					</div>
 				</div>
 				<div class="bb-custom-side page-layout-3">
@@ -83,6 +92,7 @@ function generateContentTemplate(item, index) {
  `;
 }
 
+
 function createBookObject(item, index) {
 			
 	const bookObject = {
@@ -100,13 +110,14 @@ function createBookObject(item, index) {
             }
         })(),
 		bookreader: item.accessInfo.webReaderLink || "",
+		imageLinks: item.volumeInfo.imageLinks ? true:false,
 		country: item.accessInfo.country || "",
 		downloadLink: item.accessInfo.epub.downloadLink || "",
 		pdfLink: JSON.stringify(item.accessInfo.pdf.downloadLink || ""),
 		authors: item.volumeInfo.authors || "",
 		publisher: item.volumeInfo.publisher || "",
 		thumbnail: JSON.stringify(item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : "") || "",
-		imagecover: `http://books.google.com/books/content?id=${item.id}&printsec=frontcover&img=1&zoom=1&edge=curl`|| "",
+		imagecover: `http://books.google.com/books/content?id=${item.id}&printsec=frontcover&img=1&zoom=10&edge=curl`|| "",
 		featureImage: `https://books.google.com/books/publisher/content?id=${item.id}&printsec=frontcover&img=1&zoom=6&edge=cur` || "",
 		pageCount: item.volumeInfo.pageCount || "",
 		publishDate: item.volumeInfo.publishedDate || "",
@@ -115,9 +126,9 @@ function createBookObject(item, index) {
 		etag: item.etag || "",
 		sale: item.saleInfo.saleability || "",
 		averageRating: item.volumeInfo.averageRating || ""
-	
+	    
 	};
-	
+
    return bookObject;
 }
 
@@ -149,7 +160,7 @@ const submitSearchForm = () => {
 };
 
 const getDefaultSearchValue = async () => {
-	const default_data_query ="rats+prize+zus+corpus+investor+edmonton+project+tabs+barry+online+para";
+	const default_data_query ="rats+prize+car+turtle+investor+lion+fish+coding+barry+online+money";
     const data_query = default_data_query;
     await fetchDataAndRender(data_query);
     createBookshelfScript();
@@ -208,26 +219,46 @@ async function fetchDataAndRender(data_query) {
 }
 
 async function changeBookBackground(item,index){
-	return new Promise((resolve) => {
-	const bookitem = document.querySelector(`.book[data-book="book-${index}"] .front`);
+	return new Promise((resolve) => {	
+	const emptyspace = "󠀠󠁪󠀪                    ";
+	const bookitem = document.querySelector(`.book[data-book="book-${index+1}"] .front`);
+	const booksidecover = document.querySelector(`.book[data-book="book-${index+1}"] .cover`);
+	const bookitemcontent = document.querySelector(`#book-${index+1} .bb-custom-side.page-layout-3:last-child`);
+
+	booksidecover.setAttribute('cover-data-text',`${item.title} ${emptyspace} ${item.authors[0]}`);
+
+	console.log(item.imageLinks)
+
+	if(item.imageLinks===false){
+		bookitem.classList.add('nocover');
+		bookitem.setAttribute('no-image-cover',`${item.title}`);
+	}
+	
 
 	if (bookitem) {	
 		bookitem.classList.add('book-background');
-				
-		bookitem.style.background = `linear-gradient(to right, rgba(0, 0, 0, 0.1) 0%, rgba(211, 211, 211, 0.1) 5%, rgba(255, 255, 255, 0.15) 5%, rgba(255, 255, 255, 0.1) 9%, rgba(0, 0, 0, 0.01) 100%), url(${item.imagecover}), rgb(55 63 67)`;
-
+		
+		if(item.imageLinks===false){
+			bookitem.style.background = 'linear-gradient(to right, rgba(0, 0, 0, 0.1) 0%, rgba(211, 211, 211, 0.1) 5%, rgba(255, 255, 255, 0.15) 5%, rgba(255, 255, 255, 0.1) 9%, rgba(0, 0, 0, 0.01) 100%), rgb(233 201 143)';
+		}else{
+			bookitem.style.background = `linear-gradient(to right, rgba(0, 0, 0, 0.1) 0%, rgba(211, 211, 211, 0.1) 5%, rgba(255, 255, 255, 0.15) 5%, rgba(255, 255, 255, 0.1) 9%, rgba(0, 0, 0, 0.01) 100%), url(${item.imagecover}), rgb(55 63 67)`;
+		}
+		
+		
+		bookitemcontent.style.background = `linear-gradient(to right, rgba(0, 0, 0, 0.1) 0%, rgba(211, 211, 211, 0.1) 5%, rgba(255, 255, 255, 0.15) 5%, rgba(255, 255, 255, 0.1) 9%, rgba(0, 0, 0, 0.01) 100%), url(${item.imagecover}), rgb(55 63 67)`;
 		// Set background size to 100%
 		bookitem.style.backgroundSize = '100% 100%';
-
+        bookitemcontent.style.backgroundSize = '100% 100%';
 		// Set background position to center center
 		bookitem.style.backgroundPosition = 'center center';
-
+        bookitemcontent.style.backgroundPosition = 'center center';
 		// Set background repeat to no-repeat
 		bookitem.style.backgroundRepeat = 'no-repeat';
+		bookitemcontent.style.backgroundRepeat = 'no-repeat';
 	}
 	setTimeout(() => {
 		resolve();
-	}, 1000);
+	}, 500);
     });
 }
 
